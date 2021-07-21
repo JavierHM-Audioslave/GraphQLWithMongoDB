10dcd634a7909e32817aa02bb4e69b8ae229aa12
Javier Monardo <javiermonardo@weplan-latam.com>
2021-07-21 18:06:59 -0300

    Opening connection, refactoring and creating Author and Book models

--- server/app.js
+++ server/app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const {graphqlHTTP} = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({
-    schema: schema,
-    graphiql: true
-}));
+mongoose.connect("mongodb+srv://javo:test123@author-book.llpfd.mongodb.net/Author-Book?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection
+    .once("open", () => {
+        console.log("Connected to database");
 
-app.listen(4000, () => {
-    console.log("Now listening for requests on port 4000");
-});
+        app.use("/graphql", graphqlHTTP({
+            schema: schema,
+            graphiql: true
+        }));
+
+        app.listen(4000, () => {
+            console.log("Now listening for requests on port 4000");
+        });
+    })
+    .on("error", error => {
+        console.error(`Connection refused - ${error}`);
+    });
