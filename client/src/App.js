81f132c62a9caeab0869d60a1bfe655c53015f51
Javier Monardo <javiermonardo@weplan-latam.com>
2021-07-25 20:44:50 -0300

    FRONT: first component created before installing Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
+import BookList from "./components/BookList"
 
 function App() {
   return (
     <div className="main">
-
+      <h1>Practicando GraphQL</h1>
+      <BookList/>
     </div>
   );
 }
