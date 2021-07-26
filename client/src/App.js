c02321139d578e18324a185577fc150a422e6e36
Javier Monardo <javiermonardo@weplan-latam.com>
2021-07-26 10:13:52 -0300

    BACK: apollo-boost, react-apollo and graphql dependencies installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import BookList from "./components/BookList"
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+//Apollo Client setup
+const client = new ApolloClient({ // NOTA: de esta forma Apollo Client sabe en qué endpoint ejecutará las queries que hagamos. //
+  uri: "http://localhost:4000/graphql"
+});
 
 function App() {
   return (
-    <div className="main">
-      <h1>Practicando GraphQL</h1>
-      <BookList/>
-    </div>
+    <ApolloProvider client={client}>  {/* NOTA: este ApolloProvider permite envolver todo el código React para que, dentro de éste, se pueda ejecutar las queries desde cualquier componente. // */}
+      <div className="main">
+        <h1>Practicando GraphQL</h1>
+        <BookList/>
+      </div>
+    </ApolloProvider>
   );
 }
 
